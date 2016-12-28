6b4dd4b502d645eda4fcdd1e235498b031889fb7
Aipo <p.vaitonis@gmail.com>
2016-12-28 17:23:05 +0200

    added gulpfile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,33 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
+      <link href='{{ elixir('css/app.css') }}'>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    <a href="{{ url('/login') }}">Login</a>
-                    <a href="{{ url('/register') }}">Register</a>
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-            </div>
-        </div>
-        <div id="app">
+        <div class='container' id="app">
   <p>@{{ message }}</p>
 </div>
         <script src="https://unpkg.com/vue/dist/vue.js"></script>
