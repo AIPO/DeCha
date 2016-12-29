af057c66658a872a60b770d66662e1c16be355eb
Aipo <p.vaitonis@gmail.com>
2016-12-29 12:07:28 +0200

    added v-clock updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-      <link href='{{ elixir('css/app.css') }}'>
-    </head>
-    <body>
-        <div class='container' id="app">
-  <p>Hallo @{{ name}} !</p>
-</div>
-        <script src="https://unpkg.com/vue/dist/vue.js"></script>
-        <script>
-        var app = new Vue({
-  el: '#app',
-  data: {
-    name: 'Povilas'
-  }
-})
-        </script>
-    </body>
+<head>
+  <link href='{{ elixir('css/app.css') }}'>
+</head>
+<body>
+  <div v-cloak class='container' id="app" >
+    <p v-cloak>Hallo @{{ name}} !</p>
+    <input type='text' v-model='name'>
+  </div>
+  <script src="https://unpkg.com/vue/dist/vue.js"></script>
+  <script>
+  var app = new Vue({
+    el: '#app',
+    data: {
+      name: 'Povilas'
+    }
+  })
+  </script>
+</body>
 </html>
