e5d7ff3c1138fdc167b997691a9bf5a20ad657f2
Aipo <p.vaitonis@gmail.com>
2016-12-30 14:02:44 +0200

    added v-class for done markup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <link href='{{ elixir('css/app.css') }}'>
+  <link rel="stylesheet" href='{{ elixir('css/app.css') }}'>
 </head>
 <body>
   <div v-cloak class='container' id="app" >
-    <p>Hallo @{{ name}} !</p>
-    <input type='text' v-model='name'>
+    <div class="row">
+      <div class="col-sm-12">
+        <h1>My Todos</h1>
+        <ul class="list-group" v-if="todos.length > 0">
+          <li class="list-group-item"
+          v-bind:class="{completed : todo.completed}"
+          v-for="todo in todos">
+          @{{ todo.title }}
+            <button class="btn btn-danger btn-xs pull-right" v-on:click="deleteTodo(todo)">Delete</button>
+          </li>
+        </ul>
+        <div v-else>
+          <p>
+            You don't have Todos
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12">
+        <form v-on:submit.prevent="addNewTodo(newTodo)">
+          <div class="form-group">
+            <input type="text" v-model="newTodo.title" class="form-control" placeholder="Add new Todo"/>
+          </div>
+          <div class="form-group">
+            <button class="btn btn-success">Add Todo</button>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
   <script src="https://unpkg.com/vue/dist/vue.js"></script>
   <script>
   var app = new Vue({
     el: '#app',
     data: {
-      name: 'Povilas'
+      todos: [{id:1, title:'1st task', completed:true}],
+      newTodo:{id:null, title:"", completed:false}
+    },
+    methods:{
+      addNewTodo(newTodo){
+        this.todos.push(newTodo)
+        this.newTodo = {id:null, title:"", completed:false}
+      },
+
+    deleteTodo(todo){
+  this.todos.splice(todo, 1)
+      }
     }
   })
   </script>
