b3705788ff92ecf1acf150995a1cac264a1dc42f
Aipo <p.vaitonis@gmail.com>
2017-01-02 13:53:59 +0200

    created working component

--- resources/views/welcome1.blade.php
+++ resources/views/welcome1.blade.php
@@ -1,27 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <link rel="stylesheet" href='{{ elixir('css/app.css') }}'>
+    <link rel="stylesheet" href='{{ elixir('css/app.css') }}'>
 </head>
 <body>
-  <div v-cloak class='container' id="app" >
+<div v-cloak class='container' id="app">
     <div class="row">
-      <div class="col-sm-12">
-
-      </div>
+        <div class="col-sm-12">
+<todo-item></todo-item>
+        </div>
     </div>
-  </div>
+</div>
 
-  <script src="https://unpkg.com/vue/dist/vue.js"></script>
-  <script>
-  var todoItems = Vue.extend({
-    template: '<span> This is todo list.</span>'
-  })
+<script src="https://unpkg.com/vue/dist/vue.js"></script>
+<script>
+    var TodoItems = Vue.extend({
+        template: '<span> This is todo list.</span>'
+    })
+    Vue.component('todo-item', TodoItems);
 
-
-new Vue({
-  el:"#app",
-})
-  </script>
+    new Vue({
+        el: "#app",
+    })
+</script>
 </body>
 </html>
