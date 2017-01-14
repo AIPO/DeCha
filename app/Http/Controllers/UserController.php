7936006ee195af5b2a86eb9f8478e4b213e4c61c
Aipo <p.vaitonis@gmail.com>
2017-01-14 12:57:22 +0200

    added after login screen for feed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        return view('auth.login');
+        return view('dashboard');
     }
 
     public function create()
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+Route::get('/', 'HomeController@index');
 
 Route::resource ('users', 'UserController');
 Route::get('change', 'UserController@changePassword');
